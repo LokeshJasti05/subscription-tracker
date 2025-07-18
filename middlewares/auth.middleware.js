4c8498a8ed9e3b4dd7ef322eab8b1ddbb4c21807
Lokesh Jasti <lokesh_jasti@yahoo.com>
2025-07-18 06:54:30 +0530

    finished authorization , Started ARCJET

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'; 
 import { JWT_EXPIRES_IN } from "../config/env";
+import User from '../models/user.model';
 
 const authorize = async(req,res,next) => {
     try{
@@ -13,7 +14,7 @@ const authorize = async(req,res,next) => {
 
         const decoded = jwt.verify(token, JWT_EXPIRES_IN);
 
-        const user = await User.findById(decoded.id);
+        const user = await User.findById(decoded.userId);
 
         if(!user) return res.status(401).json({message: 'Unauthorized'}); 
 
@@ -26,3 +27,4 @@ const authorize = async(req,res,next) => {
 }
 
 //1:56:41
+export default authorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import { getUsers, getUser } from '../controller/user.controller.js';
-
+import authorize from '../middlewares/auth.middleware.js';
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+
+userRouter.get('/:id',authorize, getUser);
 
 userRouter.post('/:id', (req, res) => {
     res.send({title:'Create a new user'});
